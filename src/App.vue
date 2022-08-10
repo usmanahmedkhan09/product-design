883aa28adc9ea2f8805d2f39c3abe21b13acc6c7
usman ahmed khan <usmanahmedkhan@live.com>
2022-08-11 01:39:50 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Navbar />
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import Navbar from "@/components/Navbar.vue";
+
+export default defineComponent({
+  components: { Navbar },
+  setup() {
+    return {};
+  },
+});
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="navbar">
+    <div class="nav_links">
+      <figure class="nav_logo">
+        <Logo />
+      </figure>
+      <ul>
+        <li><a href="#">Collections</a></li>
+        <li><a href="#">Men</a></li>
+        <li><a href="#">Women</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </div>
+
+    <div class="nav_profile">
+      <Cart />
+      <figure>
+        <img src="@/assets/images/image-avatar.png" alt="user image" />
+      </figure>
+    </div>
+  </nav>
+  <!-- <hr /> -->
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import Logo from "@/components/icons/IconLogo.vue";
+import Cart from "@/components/icons/IconCart.vue";
+
+export default defineComponent({
+  components: { Logo, Cart },
+  setup() {
+    return {};
+  },
+});
+</script>
